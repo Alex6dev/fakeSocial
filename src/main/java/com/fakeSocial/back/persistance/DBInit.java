dfb3e402aad34ed07e46f5ec7f0fc1fafdf412f6
Alex6dev <alexandresix2@gmail.com>
2023-06-13 14:55:50 +0200

    add data in dbInit

--- src/main/java/com/fakeSocial/back/persistance/DBInit.java
+++ src/main/java/com/fakeSocial/back/persistance/DBInit.java
@@ -24,14 +24,21 @@ public class DBInit {
         AuthInfo authInfo1= new AuthInfo("alex@gmail.com","profile123456", profile1);
         Post post1= new Post("first message", profile1, LocalDateTime.now());
         Post post2= new Post("second message", profile1, LocalDateTime.now());
+        Post post3= new Post("third message", profile1, LocalDateTime.now());
+        Post post4= new Post("fourth message", profile1, LocalDateTime.now());
+        Post post5= new Post("fifth message", profile1, LocalDateTime.now());
 
         Profile profile2 =new Profile("delage", "mathieu","France","Lille",0652524257,"mathieu@gmail.com",LocalDate.parse("1985-02-02"));
         AuthInfo authInfo2= new AuthInfo("mathieu@gmail.com","profile234567", profile2);
-        Post post3= new Post("third message", profile2, LocalDateTime.now());
-        Post post4= new Post("fourth message", profile2, LocalDateTime.now());
-
+        Post post6= new Post("sixth message", profile2, LocalDateTime.now());
+        Post post7= new Post("seventh message", profile2, LocalDateTime.now());
+        Post post8= new Post("eighths message", profile2, LocalDateTime.now());
+        Post post9= new Post("ninths message", profile1, LocalDateTime.now());
+        Post post10= new Post("tenths message", profile1, LocalDateTime.now());
         post1.addProfilLike(profile1);
         post1.addProfilLike(profile2);
+
+
 
 
         CommentModel commentModel1 = new CommentModel("first comment", profile1,LocalDateTime.now(),post1);
